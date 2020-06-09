77b48770de34ed2a445f66d37423b147f4883f14
sachalau <sachalau@gmail.com>
2020-06-09 11:18:32 +0200

    fixing docker build process

--- source/code/containers/_common/build.sh
+++ source/code/containers/_common/build.sh
@@ -7,19 +7,9 @@ IMAGE_TAG=$2
 echo "Docker Login to ECR"
 eval $(aws ecr get-login --no-include-email --region ${AWS_REGION})
 
-# retrieve image layer cache from previously built build stage
-docker pull ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} || true
-
-# (re)build just the build stage of the image
-docker build \
-    --target build \
-    --cache-from ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} \
-    --build-arg VERSION=$IMAGE_TAG \
-    -t ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} .
 
 # build the base image
 docker build \
-    --cache-from ${REGISTRY}/${IMAGE_NAME}:build-${IMAGE_TAG} \
     --build-arg VERSION=$IMAGE_TAG \
     -t $IMAGE_NAME .
 
